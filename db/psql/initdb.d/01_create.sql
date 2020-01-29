8be288f7b0d2ae7ecbc0bece51bef45e7b757917
hodanov <1031hoda@gmail.com>
2020-01-29 12:47:26 +0900

    Modify some queries for DB initialization.

--- db/psql/initdb.d/01_create.sql
+++ db/psql/initdb.d/01_create.sql
@@ -2,8 +2,7 @@ CREATE TABLE snippets (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
-    created TIMESTAMP NOT NULL,
-    expires TIMESTAMP NOT NULL
+    created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_snippets_created ON snippets(created);
